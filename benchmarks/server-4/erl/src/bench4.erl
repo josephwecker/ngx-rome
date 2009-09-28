d24dfb44dc0885812c5421233beb4da16a744cfa
Joseph Wecker <joseph.wecker@gmail.com>
2009-09-28 12:04:24 -0600

    Erlang runner correctly returning all needed data for bench.

--- benchmarks/server-4/erl/src/bench4.erl
+++ benchmarks/server-4/erl/src/bench4.erl
@@ -15,42 +15,67 @@ handle_http(Req) ->
 
 
 %---------------------------- 
-handle(["timestamp"], Req) ->
+handle([".misultin","timestamp"], Req) ->
 	Stamp = current_timestamp(),
 	Req:ok([], "~B", [Stamp]);
 
-handle(["login"], Req) ->
-	Vars = Req:parse_qs(),
+handle([".misultin","begin"], Req) ->
+	Vars = Req:parse_post(),
 	{value, {_, Username}} = lists:keysearch("username", 1, Vars),
 	NewSessID = new_uuid(),
 	SessionDict = dict:from_list([{username, Username}, {name, "Test Dude"}]),
 	ets:insert_new(sessions, {NewSessID, SessionDict}),
-	Req:ok([{"Set-Cookie", "sid=" ++ NewSessID}], "~s is awesome", [new_uuid()]);
+	Req:respond(303,
+		[{"Set-Cookie", "sid=" ++ NewSessID ++ "; path=/;"},
+		{"Location", "/welcome1.html"}], "");
 
-handle(["name"], Req) ->
+handle([".misultin","name"], Req) ->
 	SessionDict = get_session(Req),
 	{ok, Name} = dict:find(name, SessionDict),
 	Req:ok([], "~s", [Name]);
 
+handle([".misultin","name_username"], Req) ->
+	SessionDict = get_session(Req),
+	{ok, Username} = dict:find(username, SessionDict),
+	{ok, Name} = dict:find(name, SessionDict),
+	Req:ok([], "~s (~s)", [Name, Username]);
+
+handle([".misultin","username"], Req) ->
+	SessionDict = get_session(Req),
+	{ok, Username} = dict:find(username, SessionDict),
+	Req:ok([], "~s", [Username]);
+
+handle([".misultin","5divs"], Req) ->
+	D = ["first", "second", "third", "forth", "fifth"],
+	{Out,_} = lists:mapfoldl(
+		fun(Term,I) ->
+			{"<div class=\"strange\">" ++ integer_to_list(I) ++ "---&gt;" ++ Term ++ "</div>", I+1}
+		end, 1, D),
+	Req:ok([], lists:flatten(Out), []);
+			
+
 handle(_, Req) ->
-	Req:ok("page not found").
+	Req:ok("[NOT IMPLEMENTED YET]").
 
 
 %---------------------------- 
 get_session(Req) ->
 	Headers = Req:get(headers),
-	Cookies = get_cookies(Headers),
-	{value, {_, SID}} = lists:keysearch("sid", 1, Cookies),
+	{value, {_, Cookies}} = lists:keysearch('Cookie', 1, Headers),
+	Start = string:str(Cookies, "sid="),
+	SID = string:substr(Cookies, Start + 4, 32),
+	%Cookies = get_cookies(Headers),
+	%{value, {_, SID}} = lists:keysearch("sid", 1, Cookies),
 	[{_, SessionDict}] = ets:lookup(sessions, SID),
 	SessionDict.
 
 
-get_cookies(Headers) ->
-	case lists:keysearch('Cookie', 1, Headers) of
-	{value, {_, Cookies}} ->
-		mochiweb_cookies:parse_cookie(Cookies);
-	_ -> []
-	end.
+%get_cookies(Headers) ->
+%	case lists:keysearch('Cookie', 1, Headers) of
+%	{value, {_, Cookies}} ->
+%		mochiweb_cookies:parse_cookie(Cookies);
+%	_ -> []
+%	end.
 
 
 
